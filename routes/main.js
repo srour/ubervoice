c84d531b0850b3d6e78b43584caeafa40b5c2864
Ben Srour <bsrour@gmail.com>
2015-05-04 00:24:33 -0700

    response schema

--- routes/main.js
+++ routes/main.js
@@ -11,5 +11,33 @@ exports.startSession = function(req,res){
 	console.log(req.body);
 
 	//var request = JSON.parse(req.)
-	res.end();
+	res.end(createResponse({});
+}
+
+exports.createResponse = function(data){
+
+	return '{
+  "version": "1.0",
+  "sessionAttributes": {
+    "supportedHoriscopePeriods": {
+      "daily": true,
+      "weekly": false,
+      "monthly": false
+    }
+  },
+  "response": {
+    "outputSpeech": {
+      "type": "PlainText",
+      "text": "Today will provide you a new learning opportunity.  Stick with it and the possibilities will be endless."
+    },
+    "card": {
+      "type": "Simple",
+      "title": "Horoscope",
+      "subtitle": "Virgo - Daily",
+      "content": "Today will provide you a new learning opportunity.  Stick with it and the possibilities will be endless."
+    },
+    "shouldEndSession": true
+  }
+}';
+
 }
